985c70b160648b1e4912d70b77b275fce9d1a0b3
Fernando Perera <fernando02perera@gmail.com>
2023-03-16 10:41:27 +0100

    Tets(green): given_empty_string_return_zero, realizada la funcionalidad esperada del método wrap.

--- src/main/java/org/wordwrap/Wrapper.java
+++ src/main/java/org/wordwrap/Wrapper.java
@@ -4,6 +4,10 @@ public class Wrapper {
 
     public static String wrap(String textString, int columNumber){
 
+        if ( textString.isEmpty() ){
+            return "0";
+        }
+
         return textString;
 
     }
